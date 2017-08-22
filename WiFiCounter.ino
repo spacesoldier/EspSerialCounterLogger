505ebb8ff19610defd75870cbe8027d0a7bd5e17
VASoldatov <kibersoldier@gmail.com>
2017-08-22 14:39:01 +0300

    added the OTA function

--- WiFiCounter.ino
+++ WiFiCounter.ino
@@ -3,6 +3,7 @@
 #include "EspSoftwareSerial.h"
 #include <ESP8266HTTPClient.h>
 #include "FS.h"
+#include <ArduinoOTA.h>
 
 const char* ssid     = "net-xxx";
 const char* password = "xxxxxxx";
@@ -22,12 +23,32 @@ String dataWh = "";
 bool dataAwaitsTransmission = false;
 bool dataAwaitsBackup = false;
 
+void prepareOTA(){
+	ArduinoOTA.onStart([]() {
+    		Serial.println("OTA Start");
+	});
+	ArduinoOTA.onEnd([]() {
+		Serial.println("OTA End");
+		Serial.println("Rebooting...");
+	});
+  	ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    		Serial.printf("Progress: %u%%\r\n", (progress / (total / 100)));
+  	});
+	ArduinoOTA.onError([](ota_error_t error) {
+		Serial.printf("Error[%u]: ", error);
+		if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+		else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+		else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+		else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+		else if (error == OTA_END_ERROR) Serial.println("End Failed");
+	});
+	ArduinoOTA.begin();
+}
+
 void setup()
 {
 	Serial.begin(115200);
 	Serial.println("hello");
-
-	//receiver.begin(38400);
 
 	SPIFFS.begin();
 
@@ -38,10 +59,7 @@ void setup()
 	    Serial.print(".");
 	}
 
-	//Serial.println("");
-	//Serial.println("WiFi connected");
-	//Serial.println("IP address: ");
-	//Serial.println(WiFi.localIP());
+	prepareOTA();
 
 	fullUrl = String("http://");
 	fullUrl.concat(host);
@@ -129,6 +147,8 @@ void checkTime(int period, unsigned long& old_time, void callback(void), void* a
 // The loop function is called in an endless loop
 void loop()
 {
+	ArduinoOTA.handle();
+
 	checkTime(dataReadPeriodMs, old_time_dataRead, receiveData, NULL);
 	checkTime(saveDataPeriodMs, old_time_saveData, saveData, NULL);
 	checkTime(transmitDataPeriodMs, old_time_transmitData, saveData, NULL);
